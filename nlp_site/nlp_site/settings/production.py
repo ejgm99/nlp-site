a1a6b1f00b70057751cdf23a43c8937c9af794b9
ejgm99 <erickgomez@mymail.mines.edu>
2021-08-03 21:13:00 -0600

    preproduction settings setup

--- nlp_site/nlp_site/settings/production.py
+++ nlp_site/nlp_site/settings/production.py
@@ -1,8 +1,25 @@
 from nlp_site.settings.common import *
 import os
-DEBUG = False
+DEBUG = True
 
-SECRET_KEY = os.environ['SECRET_KEY']
+import secrets
+
+length = 50
+chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+SECRET_KEY = ''.join(secrets.choice(chars) for i in range(length))
+
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','ec2-3-136-18-160.us-east-2.compute.amazonaws.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'p88t90k95e99',
+        'HOST': 'general.cqx4pjfopx41.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
